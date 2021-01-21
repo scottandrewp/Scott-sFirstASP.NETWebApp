1ac9fae3db79c193b8d305693e0de22ed20b92fd
scottandrewp <70963325+scottandrewp@users.noreply.github.com>
2021-01-20 22:10:21 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scott_sFirstASP.NETWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args) //Start
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ wwwroot/js/GradeCalculator.js
@@ -0,0 +1,43 @@
+﻿var submit = document.getElementById("submitButton");
+
+submit.addEventListener("click", function () {
+    var totalGrade = parseFloat(document.getElementById("assignments").value) + parseFloat(document.getElementById("quiz").value) + parseFloat(document.getElementById("group").value) + parseFloat(document.getElementById("exam").value) + parseFloat(document.getElementById("INTEX").value);
+    var letterGrade = "";
+    if (totalGrade >= 94) {
+        letterGrade = "A";
+    }
+    else if (totalGrade < 94 && totalGrade >= 90) {
+        letterGrade = "A-";
+    }
+    else if (totalGrade < 90 && totalGrade >= 87) {
+        letterGrade = "B+";
+    }
+    else if (totalGrade < 87 && totalGrade >= 84) {
+        letterGrade = "B";
+    }
+    else if (totalGrade < 84 && totalGrade >= 80) {
+        letterGrade = "B-";
+    }
+    else if (totalGrade < 80 && totalGrade >= 77) {
+        letterGrade = "C+";
+    }
+    else if (totalGrade < 77 && totalGrade >= 74) {
+        letterGrade = "C";
+    }
+    else if (totalGrade < 74 && totalGrade >= 70) {
+        letterGrade = "C-";
+    }
+    else if (totalGrade < 70 && totalGrade >= 67) {
+        letterGrade = "D+";
+    }
+    else if (totalGrade < 67 && totalGrade >= 64) {
+        letterGrade = "D";
+    }
+    else if (totalGrade < 64 && totalGrade >= 60) {
+        letterGrade = "D-";
+    }
+    else if (totalGrade < 60) {
+        letterGrade = "E";
+    }
+    alert(`You scored ${totalGrade.toString()}%, your final grade is a(n): ${letterGrade}.`);
+});
